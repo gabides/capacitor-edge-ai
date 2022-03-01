4be82e5132d6a1e5a2aee5a418274c5c400eb411
Gabriel Dittrick <32310761+gabides@users.noreply.github.com>
2022-03-01 14:31:20 +0100

    feat(app): First commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Capacitor Edge AI',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
